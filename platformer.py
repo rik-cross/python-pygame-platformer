58a29407474f524217403ed472c51d1fca14cee8
Rik Cross <rik@raspberrypi.org>
2020-10-13 17:55:47 +0100

    Part 3 - platforms

--- platformer.py
+++ platformer.py
@@ -7,6 +7,7 @@ import pygame
 # constant variables
 SCREEN_SIZE = (700,500)
 DARK_GREY = (50,50,50)
+MUSTARD = (209,206,25)
 
 # init
 pygame.init()
@@ -15,6 +16,16 @@ pygame.display.set_caption('Rik\'s Platform Game')
 
 # player
 player_image = pygame.image.load('images/vita_00.png')
+
+# platforms
+platforms = [
+    # middle
+    pygame.Rect(100,300,400,50),
+    # left
+    pygame.Rect(100,250,50,50),
+    # right
+    pygame.Rect(450,250,50,50)
+]
 
 running = True
 while running:
@@ -28,8 +39,16 @@ while running:
     # update
 
     # draw
+
+    # background
     screen.fill(DARK_GREY)
+    # platforms
+    for p in platforms:
+        pygame.draw.rect(screen, MUSTARD, p)
+
+    # player
     screen.blit(player_image, (300,100))
+    # present screen
     pygame.display.flip()
 
 # quit
