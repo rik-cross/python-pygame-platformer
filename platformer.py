19a3d68c364eec5bbfc9774607847752b702223f
Rik Cross <rik@raspberrypi.org>
2021-03-15 17:42:56 +0000

    Moved credits into separate file for now

--- platformer.py
+++ platformer.py
@@ -1,22 +1,3 @@
-# Dinosaur player images by Arks
-# https://arks.itch.io/dino-characters
-# Twitter: @ScissorMarks
-
-# Coin sprite by DasBilligeAlien
-# https://opengameart.org/content/rotating-coin-0
-
-# Enemy sprite by bevouliin.com
-# https://opengameart.org/content/bevouliin-free-ingame-items-spike-monsters
-
-# Heart sprite by Nicole Marie T
-# https://opengameart.org/content/heart-1616
-
-# Music by ArcOfDream
-# https://arcofdream.itch.io/monolith-ost
-
-# Sound by Maskedsound
-# https://maskedsound.itch.io/8-bit-sfx-pack
-
 import pygame
 import engine
 import utils
