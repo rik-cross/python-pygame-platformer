ea93748aa142a3d14d948bc6014c98d125bba1c3
Rik Cross <rik@raspberrypi.org>
2021-04-27 22:18:13 +0100

    Added some additional tile types

--- game_tiles.py
+++ game_tiles.py
@@ -1,4 +1,6 @@
 import pygame
 import engine
 
-engine.Tile.addTile('platform', engine.Tile(pygame.image.load('images/textures/platform.png'), 'platform', True))
+engine.Tile.addTile('dirt', engine.Tile(pygame.image.load('images/textures/dirt.png'), 'platform', True))
+engine.Tile.addTile('grass', engine.Tile(pygame.image.load('images/textures/grass.png'), 'platform', True))
+engine.Tile.addTile('water', engine.Tile(pygame.image.load('images/textures/water.png'), 'platform', False))
